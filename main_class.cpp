cbdfa82c9a803edb86fe83a86a84c960faad424f
stelro <stelmach.ro@gmail.com>
2016-01-21 02:22:59 +0200

    working with initializer classes

--- main_class.cpp
+++ main_class.cpp
@@ -1,1 +1,51 @@
 #include "main_class.h"
+
+#define WINDOW_POS_X 100
+#define WINDOW_POS_Y 100
+#define WINDOW_WIDTH_PX 1024
+#define WINDOW_HEIGHT_PX 768
+
+MainClass::MainClass() {
+    quit_state = false;
+    window = nullptr;
+    window = SDL_CreateWindow("RPG",WINDOW_POS_X,
+    WINDOW_POS_Y,
+    WINDOW_WIDTH_PX,
+    WINDOW_HEIGHT_PX,
+    SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
+
+    if (window == nullptr) {
+        std::cerr << "Could not create window: " << SDL_GetError();
+        quit_state = true;
+        return 1;
+    }
+
+    renderer = nullptr;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    main_event = new SDL_Evenet();
+
+}
+
+MainClass::MainClass(const bool flag, const string &title, const uint32_t pos_x, const uint32_t pos_y, const uint32_t w, const u_int32_t, const string &window_state) {
+
+    quit_state = flag;
+    window = nullptr;
+    window = SDL_CreateWindow(title,pos_x,pos_y,w,h,window_state);
+
+    if ( window == nullptr) {
+        std::cerr << "Could not create window: " << SDL_GetError();
+        quit_state = true;
+        return 1;
+    }
+
+    renderer = nullptr;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    main_event = new SDL_Evenet();
+
+}
+
+void MainClass::GameLoop() {
+
+}
--- main_class.h
+++ main_class.h
@@ -1,4 +1,22 @@
 #ifndef GUARD_MAIN_CLASS_H
 #define GUARD_MAIN_CLASS_H
 
+#include <SDL.h>
+#include "error_handling.h"
+
+//TODO: copy constructor
+
+class MainClass {
+public:
+    MainClass();
+    MainClass(const bool flag, const string &title, const uint32_t pos_x, const uint32_t pos_y, const uint32_t w, const u_int32_t h,const string &window_state);
+    ~MainClass();
+    void GameLoop();
+private:
+    bool quit_state;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event *main_event;
+};
+
 #endif
